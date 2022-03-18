25090d55b15175672285eb539ecc6225e0ee588b
mrmurilo75 <mrmurilo75@gmail.com>
2022-03-18 18:18:06 +0000

    fix menu title underscores to spaces

--- djangocon_2022/site/views.py
+++ djangocon_2022/site/views.py
@@ -7,7 +7,7 @@ from config.settings.base import APPS_DIR
 def default_view(request, menu='home', submenu=None):
     path = APPS_DIR.__str__() + '/content/' + menu + ('/' + submenu if submenu else '')
     page = ''
-    ctx = dict(menu=menu.capitalize() if not submenu else submenu.capitalize())
+    ctx = dict(menu=(menu if not submenu else submenu).capitalize().replace('_', ' '))
     files = []
 
     for dirpath, dirname, filenames in walk(path):
