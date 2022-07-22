09da5df0215d18344a0bbe6533915a6eaec7b73e
magamig <magamig@gmail.com>
2022-07-22 15:58:49 +0200

    fix for embedded website in venueless

--- config/settings/base.py
+++ config/settings/base.py
@@ -139,7 +139,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.common.BrokenLinkEmailsMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    #"django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 # STATIC
